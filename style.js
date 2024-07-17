b282c5080cc971d9564a5e5ac4d58eebf0437733
Weslley Mateus <53063885+weslleymateus138@users.noreply.github.com>
2024-07-17 09:19:26 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll('.section');
+
+    const observerOptions = {
+        threshold: 0.1
+    };
+
+    const sectionObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    sections.forEach(section => {
+        sectionObserver.observe(section);
+    });
+});
